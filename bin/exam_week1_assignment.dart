f9708f2ef06b0d629af2ef4efc26041a92186992
Irfan Ahmad Alvi <irfanahmadalvi19@gmail.com>
2025-04-04 00:13:34 +0600

    commit

---
+++ bin/exam_week1_assignment.dart
@@ -0,0 +1,5 @@
+import 'package:exam_week1_assignment/exam_week1_assignment.dart' as exam_week1_assignment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exam_week1_assignment.calculate()}!');
+}
