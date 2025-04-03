0bf985464ed467ac7300a55551580652283ade88
Irfan Ahmad Alvi <irfanahmadalvi19@gmail.com>
2025-04-04 00:23:03 +0600

    commit

--- bin/exam_week1_assignment.dart
+++ bin/exam_week1_assignment.dart
@@ -1,5 +1,31 @@
-import 'package:exam_week1_assignment/exam_week1_assignment.dart' as exam_week1_assignment;
+//Abstract class Vehicle
+abstract class Vehicle {
+  int _speed = 0;
 
-void main(List<String> arguments) {
-  print('Hello world: ${exam_week1_assignment.calculate()}!');
+  // Abstract method
+  void move();
+
+  // Non-abstract method to set speed
+  void setSpeed(int speed) {
+    this._speed = speed;
+  }
+
+  // Getter for speed to enable encapsulation
+  int get speed => _speed;
+}
+
+//Subclass Car that extends Vehicle
+class Car extends Vehicle {
+  @override
+  void move() {
+    print("The car is moving at ${_speed} km/h");
+  }
+}
+
+void main() {
+  //Create an object of Car, set the speed, and call move()
+  Car myCar = Car();
+  myCar.setSpeed(120);
+  myCar.move();
+
 }
